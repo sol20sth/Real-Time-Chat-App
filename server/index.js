c6f04a36e8058fe65ac4c99621838ade411b0faa
김태형 <sol20s@naver.com>
2023-07-31 21:36:23 +0900

    [BE]new 실시간 채팅 구현 프로젝트 : 기본베이스 + 회원가입기능 추가

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const userRoute = require("./Routes/userRoute");
+
+const app = express();
+require("dotenv").config();
+
+app.use(express.json());
+app.use(cors());
+app.use("/api/users", userRoute);
+
+app.get("/", (req, res) => {
+  res.send("Welcome our chat app API");
+});
+
+const port = process.env.PORT || 5000;
+const uri = process.env.ATLAS_URI;
+
+app.listen(port, (req, res) => {
+  console.log(`Server running on port...: ${port}`);
+});
+
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connection established"))
+  .catch((err) => console.log("MongoDB connection failed: ", err.message));
